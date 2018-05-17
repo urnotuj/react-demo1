7c1b1a743db55b9733b3e40e4686edd96c55585b
urnotuj <zhangwei941017@163.com>
2018-05-17 19:15:02 +0800

    订单页

---
+++ src/page/order/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class OrderList extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return(<div>orderlist</div>);
+	}
+}
+
+export default OrderList;
