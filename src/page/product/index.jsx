222b9e63a6fac15f3997ba859a5aec00d26c04ac
urnotuj <zhangwei941017@163.com>
2018-05-17 19:15:29 +0800

    商品页

---
+++ src/page/product/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ProductList extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return (<div>Product</div>);
+	}
+}
+
+export default ProductList;
